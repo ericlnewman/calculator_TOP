6634bdec2b0adb51daae2ea34f72616a9e899605
ericlnewman <eric.leon.newman@gmail.com>
2024-01-25 21:54:43 -0500

    updated js and css to have a basic framework for the calculator, i.e. an input and output as well as the buttons for calculating

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,23 @@
 /*
     JavaScript for calculator project for the odin project (TOP)
 */
+const container = document.querySelector("#container");
+const flexContainer = document.createElement("div");
+flexContainer.id = "flex";
+const inputHolder = document.createElement("input");
+const outputHolder = document.createElement("output");
+const numberBox = document.createElement("div");
+inputHolder.className = "holder";
+outputHolder.className = "holder";
+numberBox.id = "numberbox";
+flexContainer.appendChild(inputHolder);
+flexContainer.appendChild(outputHolder);
+flexContainer.appendChild(numberBox);
+for(let i = 1; i <=20; i++){
+    const btn = document.createElement("button");
+    btn.id = i;
+    btn.className = "buttons";
+    numberBox.appendChild(btn);
+}
+
+container.appendChild(flexContainer);
